c2e77705779777f6dd59c43b228bc20a4255e43e
S571736 <Sondre.Gjesdal@gmail.com>
2019-02-07 11:49:02 +0100

    skriv brett

--- src/Brett.java
+++ src/Brett.java
@@ -5,4 +5,16 @@ public class Brett {
     public Brett() {
         brett = new char[3][3];
     }
+
+
+    public void skrivBrett(Brett brett){
+        System.out.println("-----------");
+
+        for (int i = 0; i < 3; i++) {
+            System.out.println("|");
+            for (int j = 0; j < 3; j++) {
+                System.out.println(brett[i][j] + "");
+            }
+        }
+    }
 }
